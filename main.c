78c536500e75155933009d4604ca8b33a5647e21
SAMIRA12CHAHDI <chahdisamira589@gmail.com>
2022-08-08 10:08:45 +0100

    ex corrige

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int factorielle(int A)  {
+	int F=1,i;
+	 for ( i=1 ; i<=A ; i++) 		 
+         F=F*i;
+
+        return F;
+	 }
+	 
+int main() {
+	
+		
+	int N , i , F,Fact;
+
+
+    printf("saisir un  nombre pour calculer sa factorielle \n ");
+    scanf("%d",&N);
+    
+   for ( i=1 ; i<=N ; i++) 	{
+   	  F=F*i;
+      
+   }
+     printf("le factorielle de %d ", F);	 
+         printf("saisir un  nombre pour calculer sa factorielle \n ");
+    scanf("%d",&N);
+       
+     Fact = factorielle(N); 
+        printf("le factorielle de %d ", Fact);
+        
+	return 0;
+}
